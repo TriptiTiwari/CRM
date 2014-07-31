059a7a19dd5b6a9ccb65410acb01d8909372a8a2
lalitbadule <lalitbadule@gmail.com>
2014-07-31 19:05:37 +0530

    Dealer Fun Query
    Function & Query Changes

---
+++ DB_CRM Query.sql
@@ -0,0 +1,23 @@
+Create Database DB_CRM
+
+
+Create Table tbl_DealerEntry
+(
+	ID int identity(1,1) constraint pk_DealerEntryID primary key(ID) not null,
+	DealerEntryID nvarchar(MAX) not null,
+	DealerFirstName nvarchar(MAX) not null,
+	DealerLaseName nvarchar(MAX) not null,
+	DateOfBirth datetime not null,
+	MobileNo nvarchar(MAX) not null,
+	PhoneNo nvarchar(MAX) not null,
+	DealerAddress nvarchar(MAX) not null,
+	City nvarchar(MAX) not null,
+	Zip nvarchar(MAX) not null,
+	DState nvarchar(MAX) not null,
+	NoOfMonths nvarchar(MAX) not null,
+	Country nvarchar(MAX) not null,
+	S_Status nvarchar(MAX)not null,
+	C_Date DateTime not null,
+)
+drop table tbl_DealerEntry
+select  * from tbl_DealerEntry
