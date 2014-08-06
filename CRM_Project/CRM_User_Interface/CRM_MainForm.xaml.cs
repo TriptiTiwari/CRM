924d5d91b28fbea47ba4b0e1fad2e3b8cefe28fa
Naziatabassum <naziatabassum317@gmail.com>
2014-08-06 19:01:18 +0530

    hjbjb
    khk

--- CRM_Project/CRM_User_Interface/CRM_MainForm.xaml.cs
+++ CRM_Project/CRM_User_Interface/CRM_MainForm.xaml.cs
@@ -57,6 +57,7 @@ namespace CRM_User_Interface
         string  bpg;
         int fetcdoc;
         List<string> checkedStuff;
+        static DataTable dtstat = new DataTable();
 
 
         BAL_Pre_Procurement bpreproc = new BAL_Pre_Procurement();
@@ -3083,6 +3084,60 @@ namespace CRM_User_Interface
    private void cmbInvoice_Tax1_DropDownClosed(object sender, EventArgs e)
    {
        calculatetax();
+   }
+public void clearAllAddedProducts()
+   {
+  
+   }
+   private void btninvoice_addProduct_Click(object sender, RoutedEventArgs e)
+   {
+       if (dtstat.Rows.Count == 0)
+       {
+           dtstat.Columns.Add("SrNo");
+           dtstat.Columns.Add("Products");
+           dtstat.Columns.Add("RatePer_Product");
+           dtstat.Columns.Add("Qty");
+           dtstat.Columns.Add("Total_Price");
+           dtstat.Columns.Add("Taxes %");
+           dtstat.Columns.Add("SubTotal");
+       }
+
+       DataRow dr = dtstat.NewRow();
+      // dr["SrNo"] = lblinvoiceSr.
+       dr["Products"] = cmbInvoiceStockProducts .SelectedValue .ToString ();
+       dr["RatePer_Product"] = txtInvoiceActualPrice .Text;
+       dr["Qty"] = txtInvoice_Qty .Text;
+       dr["Total_Price"] = txtInvoice_TotalPriceofQty .Text;
+       dr["Taxes %"] = cmbInvoice_Tax1 .Text;
+       dr["SubTotal"] = txtInvoice_SubToatal .Text;
+
+       dtstat.Rows.Add(dr);
+
+       //txtSrNo.Text = (Convert.ToInt32(txtSrNo.Text) + 1).ToString();
+      // txtProduct.Text = "";
+      // cmb1();
+       //cmb2();
+       //cmbtShortCode.SelectedIndex = 1;
+       //txtQty.Text = "";
+      // txtRateAndUnit.Text = "";
+       //txtSubTotal.Text = "";
+
+       Dgrd_InvoiceADDProducts.ItemsSource = dtstat.DefaultView;
+
+       if (dtstat.Rows.Count > 0)
+       {
+           double invamt = 0.00;
+           foreach (DataRow drow in dtstat.Rows)
+           {
+
+               invamt += Convert.ToDouble(drow["SubTotal"].ToString());
+           }
+
+           txtInvoice_InvcTotalAmount.Text = Convert.ToString(invamt);
+
+       }
+
+            
    }
 
 
