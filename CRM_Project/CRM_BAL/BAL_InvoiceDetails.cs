efc166593c049121527be844d9b937347677d402
lalitbadule <lalitbadule@gmail.com>
2014-08-07 19:00:38 +0530

    sd

---
+++ CRM_Project/CRM_BAL/BAL_InvoiceDetails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_BAL
+{
+  public   class BAL_InvoiceDetails
+    {
+      public int Flag { get; set; }
+
+      public int Customer_ID { get; set; }
+      public string Bill_No { get; set; }
+
+      public int Domain_ID { get; set; }
+      public int Product_ID { get; set; }
+      public int Brand_ID { get; set; }
+      public int P_Category { get; set; }
+      public int Model_No_ID { get; set; }
+      public int Color_ID { get; set; }
+      public double Per_Product_Price { get; set; }
+      public double Qty { get; set; }
+      public double  C_Price{ get; set; }
+      public string Tax_Name { get; set; }
+      public double Tax { get; set; }
+      public double Total_Price { get; set; }
+
+      public string S_Status { get; set; }
+      public string C_Date { get; set; }
+
+      
+    }
+}
---
+++ CRM_Project/CRM_DAL/DAL_InvoiceDetails.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using CRM_BAL;
+
+namespace CRM_DAL
+{
+   public  class DAL_InvoiceDetails
+   {
+       public SqlConnection con = new SqlConnection(ConfigurationSettings.AppSettings["ConstCRM"].ToString());
+       SqlCommand cmd;
+       public int InvoiceDetails_Save(BAL_InvoiceDetails  balid)
+       {
+           try
+           {
+
+               con.Open();
+               cmd = new SqlCommand("SP_InvoiceDetails", con);
+               cmd.CommandType = CommandType.StoredProcedure;
+               cmd.Parameters.AddWithValue("@Flag", 1);
+               cmd.Parameters.AddWithValue("@Customer_ID", balid.Customer_ID);
+               cmd.Parameters.AddWithValue("@Bill_No", balid.Bill_No);
+               cmd.Parameters.AddWithValue("@Domain_ID", balid.Domain_ID);
+               cmd.Parameters.AddWithValue("@Product_ID", balid.Product_ID);
+               cmd.Parameters.AddWithValue("@Email_ID", balid.Brand_ID );
+               cmd.Parameters.AddWithValue("@Address", balid.P_Category );
+               cmd.Parameters.AddWithValue("@Occupation", balid.Model_No_ID );
+               cmd.Parameters.AddWithValue("@Product_Details", balid.Color_ID );
+               //  cmd.Parameters.AddWithValue("@Followup_Walkin_Option", balfp.Followup_Walkin_Option);
+
+               cmd.Parameters.AddWithValue("@Followup_Type", balid.Per_Product_Price );
+               cmd.Parameters.AddWithValue("@F_Date", balid.Qty );
+               cmd.Parameters.AddWithValue("@F_Message", balid.C_Price );
+               // cmd.Parameters.AddWithValue("@Walkins", balfp.Walkins);
+               cmd.Parameters.AddWithValue("@Folloup_Update", balid.Tax_Name );
+               cmd.Parameters.AddWithValue("@Folloup_Update", balid.Tax );
+               cmd.Parameters.AddWithValue("@Folloup_Update", balid.Total_Price );
+              
+               cmd.Parameters.AddWithValue("@S_Status", balid.S_Status);
+               cmd.Parameters.AddWithValue("@C_Date", balid.C_Date);
+               int i = cmd.ExecuteNonQuery();
+               return i;
+
+
+           }
+           catch (Exception)
+           {
+
+               throw;
+           }
+           finally { con.Close(); }
+       }
+    }
+}
